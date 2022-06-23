6ee80cd6197b66a3938cf4572d90e7ec01e94d81
mikehandley <michael.j.handley@outlook.com>
2022-06-23 07:42:05 -0700

    Plotting Blocks

---
+++ Algos/w1d2algo.js
@@ -0,0 +1,56 @@
+//Data types
+/*
+Data types can include the following:
+1. Numbers (integers and decimals (floats))-> 1, 2, 3, 99, 500, 100.  Deciamls: 3.14, 3.50
+2. Strings--> Characters and letters combined into one using quotes--> "Welcome to web fundamentals!?.%432234"
+3. Booleans--> true or false
+
+
+You can put these data types into variables. You can name your variables
+
+*/
+
+
+var name = "Michael Handley";
+var age = 29;
+var chipotlePrice = 9.50;
+var chipotle_price = 9.50;
+var isteachingtoday = true;
+
+console.log("what's up" +" " +  name)
+
+chipotlePrice -= 1;
+
+console.log(chipotlePrice);
+
+var halfMyAge = age/2;
+var twiceMyAge = age * 2;
+
+
+/*
+Data strcutures are things where we can put one or many data types inside of (we can group data types into one thing)
+
+1. Array
+*/
+
+
+var artists = ["Rakim", "Kendrick", "Nas", "J. Cole", "Biggie", "Tupac", "Chance the Rapper", "Rihanna" ]; //9 elements in this array // indexes->
+
+
+/*
+A for loop is a way for us to repeat a process a certain number of times
+
+i=0
+*/
+
+for(var i  = 0; i<=20; i++){
+    console.log(i)
+}
+
+
+
+
+
+// camelCase
+// snake_case
+// PascalCase
