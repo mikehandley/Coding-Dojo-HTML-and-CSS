b5a6b6d62026bfb924ec59878e979183da9d0e4e
mikehandley <michael.j.handley@outlook.com>
2022-06-29 12:04:32 -0700

    ButtonClicker

---
+++ Week2_JavaScript/ButtonClicker/script.js
@@ -0,0 +1,11 @@
+function removeBtn(button){
+    button.parentNode.removeChild(button);
+}
+
+function changeLoginBtn(){
+    document.getElementById("btn").textContent="Logout";
+}
+
+function alertNinja () {
+    alert("Ninja was liked");
+}
